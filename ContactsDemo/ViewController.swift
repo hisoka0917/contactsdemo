49b92526c22fca28c6705a26cee162860e52c992
kai.wang <kai.wang@manaowan.com>
2018-11-12 14:33:24 +0800

    Initial Commit

---
+++ ContactsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactsDemo
+//
+//  Created by Wang Kai on 2018/11/12.
+//  Copyright © 2018 github. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
